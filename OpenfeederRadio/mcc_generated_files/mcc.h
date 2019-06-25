52e58673ddfa11f6d5521a503e83ae3c1ab738b8
MAnzilane <anzilan@hotmail.fr>
2019-06-25 10:50:39 +0200

    debug ok

--- OpenfeederRadio/mcc_generated_files/mcc.h
+++ OpenfeederRadio/mcc_generated_files/mcc.h
@@ -50,13 +50,13 @@
 #include "pin_manager.h"
 #include <stdint.h>
 #include <stdbool.h>
+#include "uart1.h"
+#include "ext_int.h"
 #include "tmr1.h"
-#include "spi1.h"
+#include "rtcc.h"
 #include "interrupt_manager.h"
 #include "traps.h"
-#include "ext_int.h"
-#include "uart1.h"
-#include "rtcc.h"
+#include "spi1.h"
 
 #define _XTAL_FREQ  4000000UL
 
--- OpenfeederRadio/mcc_generated_files/system.c
+++ OpenfeederRadio/mcc_generated_files/system.c
@@ -46,13 +46,13 @@
 #include "clock.h"
 #include "system.h"
 #include "stdint.h"
+#include "uart1.h"
+#include "ext_int.h"
 #include "tmr1.h"
-#include "spi1.h"
+#include "rtcc.h"
 #include "interrupt_manager.h"
 #include "traps.h"
-#include "ext_int.h"
-#include "uart1.h"
-#include "rtcc.h"
+#include "spi1.h"
 
 void SYSTEM_Initialize(void)
 {
@@ -62,8 +62,8 @@ void SYSTEM_Initialize(void)
     RTCC_Initialize();
     SPI1_Initialize();
     UART1_Initialize();
-    TMR1_Initialize();
     EXT_INT_Initialize();
+    TMR1_Initialize();
 }
 
 /**
